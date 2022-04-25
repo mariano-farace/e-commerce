e5582ed7f17a005e858c337994c6e7287093c161
Mariano <mariano_farace@hotmail.com>
2022-04-25 07:26:18 +0200

    Add key from id

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -93,7 +93,7 @@ export const Slider = () => {
       </Arrow>
       <Wrapper slideIndex={slideIndex}>
         {sliderItems.map((item) => (
-          <Slide bg={item.bg}>
+          <Slide bg={item.bg} key={item.id}>
             <ImageContainer>
               <Image src={item.img}></Image>
             </ImageContainer>
