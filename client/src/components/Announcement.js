8d6e815a9d6a92e1110445bf4c5520a3fcdba458
Mariano <mariano_farace@hotmail.com>
2022-04-25 07:54:09 +0200

    Fix issue with  Styled(Component) created dynamically

--- client/src/components/Announcement.js
+++ client/src/components/Announcement.js
@@ -1,17 +1,16 @@
 import styled from "styled-components";
 
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+  color: white;
+`;
 const Announcement = () => {
-  const Container = styled.div`
-    height: 30px;
-    background-color: teal;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 14px;
-    font-weight: 500;
-    color: white;
-  `;
-
   return (
     <div>
       <Container>Super Deal: Free Shippin to Spain!</Container>
