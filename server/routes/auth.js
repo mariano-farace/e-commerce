d39cb288bc4366d886a57816b446acef1b1c2859
Mariano <mariano_farace@hotmail.com>
2022-05-14 12:31:29 +0200

    Implement argon to register

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,16 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const { hashPassword } = require("../helpers");
 
-//TODO error handling: si ya existe el usuario, no se puede crear
+// TODO error handling: si ya existe el usuario, no se puede crear
 
 router.post("/register", async (req, res) => {
+  const passwordHashed = await hashPassword(req.body.password);
+
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: passwordHashed,
   });
 
   try {
