146a930368f33f1a994d88413c1217a8430c8bfc
Mariano <mariano_farace@hotmail.com>
2022-05-15 11:45:44 +0200

    Implement update user info handler

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const { verifyTokenAndAuthorization } = require("./verifyJWT");
+const { hashPassword } = require("../helpers");
 
-router.get("/", (req, res) => {
-  res.send("User test successful");
+// Change user details 1h
+// TODO esto hay que revisarlo, porque si se le ocurre mandar isAdmin desde el front, podrian cambiarlo, definitivamente hay que modificarlo
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = await hashPassword(req.body.password);
+  }
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      // TODO cambiar este metodo $set por uno mas apropiado
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).json({ message: err });
+  }
 });
 
 module.exports = router;
