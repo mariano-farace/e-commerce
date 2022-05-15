9172cb7c61114079a1b332108a82cea53cf70bfe
Mariano <mariano_farace@hotmail.com>
2022-05-15 11:45:30 +0200

    Implement JWT

---
+++ server/routes/verifyJWT.js
@@ -0,0 +1,49 @@
+const jwt = require("jsonwebtoken");
+
+// TODO usar authorization header!
+// TODO escribir esto en una carpeta middleware
+const verifyToken = (req, res, next) => {
+  // TODO escribir usando async await
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
+      if (err) {
+        return res.status(401).json({ message: "Invalid token" });
+      } else {
+        // attach user to request
+        req.user = user;
+        next();
+      }
+    });
+    next();
+  } else {
+    return res.status(401).json({ message: "Forbidden" });
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json({ message: "Forbidden" });
+    }
+  });
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json({ message: "Forbidden" });
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
