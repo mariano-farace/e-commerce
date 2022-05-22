664c6a47b89f94f99d0d5c92be3f4917ae1b4773
Mariano <mariano_farace@hotmail.com>
2022-05-22 12:59:49 +0200

    Add routes with its controllers

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const { MONGO_DB_URL, PORT } = require("./config");
 const userRouter = require("./routes/user");
 const authRouter = require("./routes/auth");
 const productRouter = require("./routes/product");
+const orderRouter = require("./routes/order");
+const cartRouter = require("./routes/cart");
 // TODO install nodemon as dev dependency
 // TODO add multer and helmet
 // TODO reestructurar los folders para dividirlos mejor
@@ -27,9 +29,12 @@ app.get("/api/v1/test", (req, res) => {
   res.send("successful test");
 });
 
+// TODO modificar para usar singular en las rutas
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/products", productRouter);
+app.use("/api/v1/carts", cartRouter);
+app.use("/api/v1/orders", orderRouter);
 
 app.listen(PORT || 3000, () => {
   console.log("Server running on port 3000");
