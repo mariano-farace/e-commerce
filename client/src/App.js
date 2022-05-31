40e068cb4001579f6f8f66df1ea4a6ab18344b13
Mariano <mariano_farace@hotmail.com>
2022-05-31 12:12:15 +0200

    Implement React router main

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,43 @@
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import ProductList from "./pages/ProductList";
 import ProductPage from "./pages/ProductPage";
 import Cart from "./pages/Cart";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  return <Cart />;
+  const user = true;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+      </Routes>
+
+      <Routes>
+        <Route path="/products/:category" element={<ProductList />}></Route>
+      </Routes>
+
+      <Routes>
+        <Route path="/product/:id" element={<ProductPage />}></Route>
+      </Routes>
+
+      <Routes>
+        <Route path="/cart" element={<Cart />}></Route>
+      </Routes>
+
+      <Routes>
+        {user ? <Navigate to="/" /> : <Login />}
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+
+      <Routes>
+        {user ? <Navigate to="/" /> : <Register />}
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
