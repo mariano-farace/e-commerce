1286d972473ed592f98587bc9c583d5d4ecac817
Mariano <mariano_farace@hotmail.com>
2022-06-01 09:56:28 +0200

    Fix routes error

--- client/src/App.js
+++ client/src/App.js
@@ -8,33 +8,22 @@ import Cart from "./pages/Cart";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  const user = true;
+  const user = false;
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
-      </Routes>
-
-      <Routes>
         <Route path="/products/:category" element={<ProductList />}></Route>
-      </Routes>
-
-      <Routes>
         <Route path="/product/:id" element={<ProductPage />}></Route>
-      </Routes>
-
-      <Routes>
         <Route path="/cart" element={<Cart />}></Route>
-      </Routes>
-
-      <Routes>
-        {user ? <Navigate to="/" /> : <Login />}
-        <Route path="/login" element={<Login />}></Route>
-      </Routes>
-
-      <Routes>
-        {user ? <Navigate to="/" /> : <Register />}
-        <Route path="/register" element={<Register />}></Route>
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/" /> : <Login />}
+        ></Route>
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
